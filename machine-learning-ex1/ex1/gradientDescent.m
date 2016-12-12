c95d56724899169b28310d9fb9ffe8442d40f6ec
kartik <venkatramankartik27@gmail.com>
2016-12-12 11:36:33 +0530

    Gradient partial

--- machine-learning-ex1/ex1/gradientDescent.m
+++ machine-learning-ex1/ex1/gradientDescent.m
@@ -8,6 +8,12 @@ m = length(y); % number of training examples
 J_history = zeros(num_iters, 1);
 
 for iter = 1:num_iters
+	for i = 1:m
+		temp_1 = theta(1) - (X(i,:)*theta - y(i))*X(i,1)*alpha/m;
+		temp_2 = theta(2) - (X(i,:)*theta - y(i))*X(i,2)*alpha/m;
+		theta(1) = temp_1;
+		theta(2) = temp_2;
+	end
 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Perform a single gradient step on the parameter vector
