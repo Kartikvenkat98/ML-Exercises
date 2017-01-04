95f5efc5e1f3290a7a3bc55c730aa7bc91561308
kartik <venkatramankartik27@gmail.com>
2017-01-05 00:26:30 +0530

    Exercise 8 done

--- machine-learning-ex8/ex8/selectThreshold.m
+++ machine-learning-ex8/ex8/selectThreshold.m
@@ -12,7 +12,14 @@ F1 = 0;
 
 stepsize = (max(pval) - min(pval)) / 1000;
 for epsilon = min(pval):stepsize:max(pval)
-    
+	predictions = (pval < epsilon);
+    	tp = sum((predictions == 1) & (yval == 1));
+    	fp = sum((predictions == 1) & (yval == 0));
+    	fn = sum((predictions == 0) & (yval == 1));
+    	prec = tp / (tp + fp);
+    	rec = tp / (tp + fn);
+    	F1 = 2 * prec * rec / (prec + rec);
+ 
     % ====================== YOUR CODE HERE ======================
     % Instructions: Compute the F1 score of choosing epsilon as the
     %               threshold and place the value in F1. The code at the
